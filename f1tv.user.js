d0de82a58342884552e2fec2e02920717c35c5b4
Joe Gibson <mrjoefgibson@gmail.com>
2022-10-21 17:47:39 -0500

    Update f1tv.user.js

--- f1tv.user.js
+++ f1tv.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Local Dev Deployment
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Add styling to compress menu bar and margin or padding above video.
 // @author       Joe
 // @match        https://f1tv.formula1.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=busites.com
 // @grant        none
 // @downloadURL  https://github.com/joegibson/userscripts/raw/main/f1tv.user.js
 // ==/UserScript==
