f500839500d9806f467b65d211930f519d4029a3
Joe Gibson <mrjoefgibson@gmail.com>
2024-08-18 14:01:51 -0500

    Update yt.user.js

--- yt.user.js
+++ yt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YT remove playlist functionality
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Removing playlist function so a video doesn't auto play the next one
 // @author       Joe
 // @match        https://www.youtube.com/playlist?list=WL
@@ -14,6 +14,7 @@
   let wlContainer = document.querySelector('#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer')
   let hrefPathToReplace = 'list=WL&index='
   let wlLinks = wlContainer.querySelectorAll("a#thumbnail,a#video-title")
+  
   wlLinks.forEach(function(l){
     let currentHref = l.getAttribute("href");
     let newHref = currentHref.replace('list=WL&index=','nothing=')
