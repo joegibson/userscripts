80b8e94c863ad7d8c10c6a1927e26f6385fcf0a7
Joe Gibson <mrjoefgibson@gmail.com>
2024-08-18 13:48:48 -0500

    Update yt.user.js

--- yt.user.js
+++ yt.user.js
@@ -1,1 +1,22 @@
+// ==UserScript==
+// @name         Local Dev Deployment
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add styling to compress menu bar and margin or padding above video.
+// @author       Joe
+// @match        https://www.youtube.com/playlist?list=WL
+// @grant        none
+// @downloadURL  https://github.com/joegibson/userscripts/raw/main/yt.user.js
+// ==/UserScript==
 
+(function() {
+  'use strict';
+  let wlContainer = document.querySelector('#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer')
+  let hrefPathToReplace = 'list=WL&index='
+  let wlLinks = wlContainer.querySelectorAll("a#thumbnail,a#video-title")
+  wlLinks.forEach(function(l){
+    let currentHref = l.getAttribute("href");
+    let newHref = currentHref.replace('list=WL&index=','nothing=')
+    l.setAttribute("href", newHref);
+  })
+})();
