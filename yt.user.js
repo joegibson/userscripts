64037aed82d0deead776a24330cbd320f0a854cb
Joe Gibson <mrjoefgibson@gmail.com>
2024-08-18 14:17:01 -0500

    Update yt.user.js

--- yt.user.js
+++ yt.user.js
@@ -1,23 +1,25 @@
 // ==UserScript==
 // @name         YT remove playlist functionality
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Removing playlist function so a video doesn't auto play the next one
 // @author       Joe
-// @match        https://www.youtube.com/playlist?*
+// @match        https://www.youtube.com/*
 // @grant        none
 // @downloadURL  https://github.com/joegibson/userscripts/raw/main/yt.user.js
 // ==/UserScript==
 
 (function() {
   'use strict';
-  let wlContainer = document.querySelector('#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer')
-  let hrefPathToReplace = 'list=WL&index='
-  let wlLinks = wlContainer.querySelectorAll("a#thumbnail,a#video-title")
-  
-  wlLinks.forEach(function(l){
-    let currentHref = l.getAttribute("href");
-    let newHref = currentHref.replace('list=WL&index=','nothing=')
-    l.setAttribute("href", newHref);
-  })
+  if(window.location.href === 'https://www.youtube.com/playlist?list=WL'){
+    let wlContainer = document.querySelector('#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer')
+    let hrefPathToReplace = 'list=WL&index='
+    let wlLinks = wlContainer.querySelectorAll("a#thumbnail,a#video-title")
+    
+    wlLinks.forEach(function(l){
+      let currentHref = l.getAttribute("href");
+      let newHref = currentHref.replace('list=WL&index=','nothing=')
+      l.setAttribute("href", newHref);
+    })
+  }
 })();
