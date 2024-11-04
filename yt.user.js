03eca3fd271047cacb6b6866a12bc36ca756243e
Joe Gibson <mrjoefgibson@gmail.com>
2024-11-04 09:38:10 -0600

    Update yt.user.js

--- yt.user.js
+++ yt.user.js
@@ -9,17 +9,15 @@
 // @downloadURL  https://github.com/joegibson/userscripts/raw/main/yt.user.js
 // ==/UserScript==
 
-addEventListener("DOMContentLoaded", (event) => {
-  if(window.location.href === 'https://www.youtube.com/playlist?list=WL'){
-    console.log("DOMContentLoaded")
-    let wlContainer = document.querySelector('#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer')
-    let hrefPathToReplace = 'list=WL&index='
-    let wlLinks = wlContainer.querySelectorAll("a#thumbnail,a#video-title")
-    
-    wlLinks.forEach(function(l){
-      let currentHref = l.getAttribute("href");
-      let newHref = currentHref.replace('list=WL&index=','nothing=')
-      l.setAttribute("href", newHref);
-    })
-  }
-});
+if(ytPageType === 'playlist'){
+  console.log("Playlist page")
+  let wlContainer = document.querySelector('#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer')
+  let hrefPathToReplace = 'list=WL&index='
+  let wlLinks = wlContainer.querySelectorAll("a#thumbnail,a#video-title")
+  
+  wlLinks.forEach(function(l){
+    let currentHref = l.getAttribute("href");
+    let newHref = currentHref.replace('list=WL&index=','nothing=')
+    l.setAttribute("href", newHref);
+  })
+}
