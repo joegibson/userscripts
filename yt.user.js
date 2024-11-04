a346bdfbacad4f5d307c75048778f3a37c524f7c
Joe Gibson <mrjoefgibson@gmail.com>
2024-11-04 09:42:50 -0600

    Update yt.user.js

--- yt.user.js
+++ yt.user.js
@@ -10,17 +10,17 @@
 // @updateURL    https://github.com/joegibson/userscripts/raw/main/yt.user.js
 // ==/UserScript==
 
-if(ytPageType === 'playlist'){
-  setTimeout(() => {
-    console.log("Playlist page")
-    let wlContainer = document.querySelector('#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer')
-    let hrefPathToReplace = 'list=WL&index='
-    let wlLinks = wlContainer.querySelectorAll("a#thumbnail,a#video-title")
-    
-    wlLinks.forEach(function(l){
-      let currentHref = l.getAttribute("href");
-      let newHref = currentHref.replace('list=WL&index=','nothing=')
-      l.setAttribute("href", newHref);
-    })
-  }, "2000");
-}
+setTimeout( () => {
+	if ( ytPageType === 'playlist' ) {
+		console.log( "Playlist page" )
+		let wlContainer = document.querySelector( '#contents.ytd-playlist-video-list-renderer.ytd-playlist-video-list-renderer' )
+		let hrefPathToReplace = 'list=WL&index='
+		let wlLinks = wlContainer.querySelectorAll( "a#thumbnail,a#video-title" )
+
+		wlLinks.forEach( function ( l ) {
+			let currentHref = l.getAttribute( "href" );
+			let newHref = currentHref.replace( 'list=WL&index=', 'nothing=' )
+			l.setAttribute( "href", newHref );
+		})
+	}
+}, "2000" );
